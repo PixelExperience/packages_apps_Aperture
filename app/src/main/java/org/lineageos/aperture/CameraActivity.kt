aeffb74d099cdd2f031da8999bc1e4be3d307262
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-12-24 19:11:25 +0100

    Aperture: Split ZSL toggle from photo capture mode
    * Also disable vendor extensions if enabled
    
    Change-Id: I72c2242595dee8ed986f8e38d3f27eff5174e6b7

--- app/src/main/java/org/lineageos/aperture/CameraActivity.kt
+++ app/src/main/java/org/lineageos/aperture/CameraActivity.kt
@@ -149,6 +149,7 @@ import java.io.FileNotFoundException
 import java.io.InputStream
 import java.util.concurrent.ExecutorService
 import kotlin.math.abs
+import kotlin.properties.Delegates
 import kotlin.reflect.safeCast
 
 @androidx.camera.camera2.interop.ExperimentalCamera2Interop
@@ -234,6 +235,9 @@ open class CameraActivity : AppCompatActivity() {
     private val isSecureCamera: Boolean
         get() = this is SecureCameraActivity
     private val secureMediaUris = mutableListOf<Uri>()
+
+    // Photo
+    private var photoCaptureMode by Delegates.notNull<Int>()
 
     // Video
     private val supportedVideoQualities: List<Quality>
@@ -1131,8 +1135,20 @@ open class CameraActivity : AppCompatActivity() {
             }
         }
 
+        photoCaptureMode = sharedPreferences.photoCaptureMode.let {
+            if (it == ImageCapture.CAPTURE_MODE_ZERO_SHUTTER_LAG && !camera.supportsZsl) {
+                // Fallback to minimize latency
+                ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY
+            } else {
+                it
+            }
+        }
+
         // Only photo mode supports vendor extensions for now
-        val cameraSelector = if (cameraMode == CameraMode.PHOTO) {
+        val cameraSelector = if (
+            cameraMode == CameraMode.PHOTO &&
+            photoCaptureMode != ImageCapture.CAPTURE_MODE_ZERO_SHUTTER_LAG
+        ) {
             cameraManager.extensionsManager.getExtensionEnabledCameraSelector(
                 camera.cameraSelector, sharedPreferences.photoEffect
             )
@@ -1167,7 +1183,7 @@ open class CameraActivity : AppCompatActivity() {
         cameraController.setEnabledUseCases(cameraUseCases)
 
         // Restore settings that needs a rebind
-        cameraController.imageCaptureMode = sharedPreferences.photoCaptureMode
+        cameraController.imageCaptureMode = photoCaptureMode
 
         // Bind camera controller to lifecycle
         cameraController.bindToLifecycle(this)
@@ -1627,7 +1643,9 @@ open class CameraActivity : AppCompatActivity() {
      */
     private fun updatePhotoEffectIcon() {
         effectButton.isVisible =
-            cameraMode == CameraMode.PHOTO && camera.supportedExtensionModes.size > 1
+            cameraMode == CameraMode.PHOTO &&
+                    photoCaptureMode != ImageCapture.CAPTURE_MODE_ZERO_SHUTTER_LAG &&
+                    camera.supportedExtensionModes.size > 1
 
         sharedPreferences.photoEffect.let {
             effectButton.setCompoundDrawablesWithIntrinsicBounds(
